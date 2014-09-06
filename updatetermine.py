4d9f859c844cc4b20d7116d8b5c2aa762d59cbc2
koebi <koebischnell@googlemail.com>
2014-09-06 20:03:16 +0200

    fertiges updatetermine

--- updatetermine.py
+++ updatetermine.py
@@ -10,7 +10,8 @@ TRENNER = "================="
 
 
 def updateCalendar():
-	data = io.open(calTxtPath, 'wrb').read().decode('utf-8')
+	f = io.open(calTxtPath, 'wrb')
+	data = f.read().decode('utf-8')
 	t = data.split('\n')
 
 	newdays = []
@@ -21,19 +22,51 @@ def updateCalendar():
 			if t[i] != TRENNER:
 				t[i] = TRENNER
 
+	# Finde alle Indizes von Tagen
 	for i in range(len(t)):
 		if t[i] == TRENNER:
-			newdays += [i]
-
-	first = t[0]
-	firstwd, firstmm, firstyy = first.split("/")
-	firstwd, firstdd = firstwd.split()
-	firstdd, firstmm, firstyy = int(firstdd), int(firstmm), int(firstyy)
-	first = datetime.date(firstyy, firstmm, firstdd)
+			newdays += [i-1]
 
 	today = datetime.date.today()
 
-	if first != today:
-		
+	for i in newdays:
+		first = t[0]
+		firstwd, firstmm, firstyy = first.split("/")
+		firstwd, firstdd = firstwd.split()
+		firstdd, firstmm, firstyy = int(firstdd), int(firstmm), int(firstyy)
+		first = datetime.date(firstyy, firstmm, firstdd)
+		if first == today:
+			first = i
+			break
 
-	
+	old = t[:i]
+	t = t[i:]
+
+	deldays = 0
+	for i in old:
+		if i[0] == "=":
+			deldays += 1
+
+	for i in range(deldays):
+		newday = today + datetime+timedelta(i+1)
+		wd = dayToString(newday.weekday())
+		dd = str(newday.day) if len(str(newday.day)) == 2 else '0' + str(newday.day)
+		mm = str(newday.month) if len(str(newday.month)) == 2 else '0' + str(newday.month)
+		yy = str(newday.year)
+		newline1 = wd + " " + dd + "/" + mm + "/" + yy
+		newline2 = TRENNER
+		newline3 = ""
+		t += [newline1, newline2, newline3]
+
+	newdata = ""
+	for i in t:
+		newdata = newdata + i + "\n"
+
+	newdata = newdata[:-2]
+
+	f.write(newdata)
+	f.close
+
+def dayToString(wd):
+	days = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
+	return days[wd]
