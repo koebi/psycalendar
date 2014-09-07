46a1bb84addb175faeeb564543a6bb8991720c0d
koebi <koebischnell@googlemail.com>
2014-09-07 10:51:14 +0200

    update Termine now working

--- updatetermine.py
+++ updatetermine.py
@@ -9,18 +9,19 @@ calTxtPath = "/home/koebi/scratch/termine.txt"
 TRENNER = "================="
 
 
-def updateCalendar():
-	f = io.open(calTxtPath, 'wrb')
-	data = f.read().decode('utf-8')
+def updateTermine():
+	with open(calTxtPath, 'rb') as f:
+		data = f.read().decode('utf-8')
 	t = data.split('\n')
 
 	newdays = []
 
 	# Ersetze jede Zeile mit = vorne durch TRENNER
 	for i in range(len(t)):
-		if t[i][0] == "=":
-			if t[i] != TRENNER:
-				t[i] = TRENNER
+		if t[i] != "":
+			if t[i][0] == "=":
+				if t[i] != TRENNER:
+					t[i] = TRENNER
 
 	# Finde alle Indizes von Tagen
 	for i in range(len(t)):
@@ -28,9 +29,10 @@ def updateCalendar():
 			newdays += [i-1]
 
 	today = datetime.date.today()
+	latest = t[newdays[-1]]
 
 	for i in newdays:
-		first = t[0]
+		first = t[i]
 		firstwd, firstmm, firstyy = first.split("/")
 		firstwd, firstdd = firstwd.split()
 		firstdd, firstmm, firstyy = int(firstdd), int(firstmm), int(firstyy)
@@ -39,16 +41,22 @@ def updateCalendar():
 			first = i
 			break
 
-	old = t[:i]
-	t = t[i:]
+	old = t[:first]
+	t = t[first:]
 
 	deldays = 0
 	for i in old:
-		if i[0] == "=":
-			deldays += 1
+		if i != "":
+			if i[0] == "=":
+				deldays += 1
+
+	latestwd, latestmm, latestyy = latest.split("/")
+	latestwd, latestdd = latestwd.split()
+	latestdd, latestmm, latestyy = int(latestdd), int(latestmm), int(latestyy)
+	latest = datetime.date(latestyy, latestmm, latestdd)
 
 	for i in range(deldays):
-		newday = today + datetime+timedelta(i+1)
+		newday = latest + datetime.timedelta(i+1)
 		wd = dayToString(newday.weekday())
 		dd = str(newday.day) if len(str(newday.day)) == 2 else '0' + str(newday.day)
 		mm = str(newday.month) if len(str(newday.month)) == 2 else '0' + str(newday.month)
@@ -64,9 +72,13 @@ def updateCalendar():
 
 	newdata = newdata[:-2]
 
+	f = open(calTxtPath, "wt")
 	f.write(newdata)
 	f.close
 
 def dayToString(wd):
 	days = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
 	return days[wd]
+
+if __name__ == "__main__":
+	updateTermine()
